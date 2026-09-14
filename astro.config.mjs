01844d9cae22767b8db404616cda74032f15861c
Luis <184137556+avilacrew369@users.noreply.github.com>
2026-09-14 00:47:34 -0500

    https://panther-text-blue-end1

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,5 +31,11 @@ export default defineConfig({
     ],
   },
 
-  adapter: netlify(),
+  adapter: netlify({
+     devFeatures: {
+      images: true,
+      environmentVariables: false,
+      edgeFunctions: false,
+    },
+  }),
 });
